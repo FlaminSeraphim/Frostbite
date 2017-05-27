c6352ed6632296a3dec69b1f8daa5dcd00713598
FlaminSeraphim <mrtnkaboom@aol.com>
2017-05-26 19:51:40 -0500

    refactored api query lines
    I made sure to add = to every query type. I also refactored the lines so that each line doesn't start with a +. Then added some semi colons.

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ app.get('/', function(req, res){
 
 //Search Route
 app.get('/search', function(req, res){
-  var url = "https://b9c19a63a96d2c303c9026abcb12bc2a@okcountyrecords.com/api/v1/search?" + "county=" + req.query.county + "&text" +
-  req.query.text + "&party_name=" + req.query.party_name + "&party_type" + req.query.party_type +"&book=" +
-  req.query.book + "&page=" + req.query.page + "&series=" + req.query.series + "&number=" + req.query.number + "&type=" + req.query.type
-  + "&indexed_date_start=" + req.query.date_start + "&indexed_date_end=" + req.query.date_end
-  + "&modified_date_start" + req.query.modified_date_start + "&modified_date_end" + req.query.modified_date_end
-  + "&results_per_page" + req.query.results_per_page + "&legal_description=" + req.query.legal_description + "&result_page"
-  + req.query.result_page + "&order_by" + req.query.order_by + "&order_direction" + req.query.order_direction;
+  var url = "https://b9c19a63a96d2c303c9026abcb12bc2a@okcountyrecords.com/api/v1/search?" + "county=" + req.query.county + "&text=" +
+  req.query.text + "&party_name=" + req.query.party_name + "&party_type=" + req.query.party_type + "&book=" +
+  req.query.book + "&page=" + req.query.page + "&series=" + req.query.series + "&number=" + req.query.number + "&type=" + req.query.type +
+  "&indexed_date_start=" + req.query.date_start + "&indexed_date_end=" + req.query.date_end +
+  "&modified_date_start=" + req.query.modified_date_start + "&modified_date_end=" + req.query.modified_date_end +
+  "&results_per_page=" + req.query.results_per_page + "&legal_description=" + req.query.legal_description + "&result_page=" +
+  req.query.result_page + "&order_by=" + req.query.order_by + "&order_direction=" + req.query.order_direction;
   request(url,function(error, response, body){
     if (error){
-      console.log(error)
+      console.log(error);
     }
     console.log(url);
     var data = JSON.parse(body);
@@ -43,7 +43,7 @@ app.get('/search', function(req, res){
 //advanced search like see images etc
 app.get('/advanced', function(req, res){
   res.render('advanced');
-})
+});
 
 var server = app.listen(3000, '0.0.0.0', function(){
   console.log('Listening to port 3000');
